7e282bee627c04b28a63251a705c3addf4f840f7
Shahnawaz Ansari <absolutez3ro.exe@gmail.com>
2020-09-29 13:26:42 +0530

    Remove "id" field from Keyword.kt data class

--- app/src/main/java/com/example/kotlinkeywords/data/Keyword.kt
+++ app/src/main/java/com/example/kotlinkeywords/data/Keyword.kt
@@ -1,11 +1,10 @@
 package com.example.kotlinkeywords.data
 
 data class Keyword(
-    val id: Int,
     val name: String,
     val description: String
 )
 
 val keywords: List<Keyword> = List(20) {
-    Keyword(1, "if", "conditional statement $it")
+    Keyword("if", "conditional statement $it")
 }
